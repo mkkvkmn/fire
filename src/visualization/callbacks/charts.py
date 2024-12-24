a17c17a29ed26fd9d9a2f79549e6cd32d5780ae7
mkkvkmn <github@feellike.io>
2024-12-24 22:30:07 +0200

    somewhat works overview bar

--- src/visualization/callbacks/charts.py
+++ src/visualization/callbacks/charts.py
@@ -1,6 +1,7 @@
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+from config.settings import SETTINGS
 
 
 def register_chart_callbacks(app):
@@ -20,6 +21,17 @@ def register_chart_callbacks(app):
         # convert data to DataFrame
         date_filtered = pd.DataFrame(data)
 
+        # define the desired classes using category_mappings from settings
+        income_class = SETTINGS["category_mappings"]["income"]
+        costs_class = SETTINGS["category_mappings"]["costs"]
+        desired_classes = [income_class, costs_class]
+
+        # filter the DataFrame to include only the desired classes
+        date_filtered = date_filtered[date_filtered["class"].isin(desired_classes)]
+
+        # convert costs to positive values
+        date_filtered.loc[date_filtered["class"] == costs_class, "amount"] *= -1
+
         # create bar chart
         fig = px.bar(
             date_filtered,
@@ -29,6 +41,15 @@ def register_chart_callbacks(app):
             barmode="group",
             labels={"period": "Period", "amount": "Amount", "class": "Category"},
             title="Income and Costs",
+            template="plotly_dark",
+        )
+
+        # style the start matching KPI's
+        fig.update_layout(
+            yaxis_tickformat=",.2f",
+            xaxis_title="Period",
+            yaxis_title="Amount",
+            legend_title="Category",
         )
 
         return fig.to_dict()
